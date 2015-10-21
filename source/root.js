64b25e5b2b13612edd15b3cc41fd46d89226892a
Giovanni Calò <giovanni.calo@live.it>
2015-10-21 02:23:41 +0100

    Added basic root component module

---
+++ source/root.js
@@ -0,0 +1,28 @@
+import Falcor from "falcor";
+import React from "react";
+
+export default function(config) {
+	return function(component) {
+		const Component = component;
+		return class FalcorRoot extends React.Component {
+
+			constructor(props) {
+				super(props);
+				this.state = { model: new Falcor.Model(config()) };
+			}
+
+			static childContextTypes = {
+				model: React.PropTypes.object.isRequired
+			};
+
+			getChildContext() {
+				return { model: this.state.model };
+			}
+
+			render() {
+				return <Component />;
+			}
+
+		};
+	};
+}
