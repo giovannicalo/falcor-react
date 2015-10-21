7e06a0088ec0ad23a971e4863b084c2d439d1662
Giovanni Calò <giovanni.calo@live.it>
2015-10-21 02:24:53 +0100

    Added test leaf component

---
+++ test/leaf.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import FalcorReact from "../source";
+
+@FalcorReact.Leaf(() => {
+	return [["foo", "bar"]];
+})
+export default class Leaf extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <div />;
+	}
+
+}
