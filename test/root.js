5deeb740690035dfe261c68d6190c4ba256e6587
Giovanni Calò <giovanni.calo@live.it>
2015-10-25 20:35:12 +0000

    Added more leaves to the test root, slightly enlarged test Falcor model

--- test/root.js
+++ test/root.js
@@ -2,18 +2,27 @@ import React from "react";
 
 import FalcorReact from "../source";
 import Leaf from "./leaf";
+import LeafSafe from "./leaf/safe";
+import LeafUnsafe from "./leaf/unsafe";
+import LeafWithProps from "./leaf/with-props";
 
 @FalcorReact.Root(() => {
-	return { cache: { foo: { bar: "foo" } } };
+	return { cache: { foo: { bar: "foo", foo: "bar" } } };
 })
 export default class Root extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = { foo: "bar" };
 	}
 
 	render() {
-		return <Leaf />;
+		return <div>
+			<Leaf />
+			<LeafSafe />
+			<LeafUnsafe />
+			<LeafWithProps foo={this.state.foo} />
+		</div>;
 	}
 
 }
