25cb5e12142e7eeed7c2d08d856b93373c790a83
Giovanni Calò <giovanni.calo@live.it>
2015-10-25 20:38:52 +0000

    Added more tests

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ import Jsdom from "jsdom";
 import React from "react";
 import ReactTestUtils from "react-addons-test-utils";
 
+import FindLeaf from "./find-leaf";
 import Root from "./root";
 
 const expect = Chai.expect;
@@ -15,16 +16,44 @@ global.document = Jsdom.jsdom();
 global.window = global.document.defaultView;
 
 const tree = ReactTestUtils.renderIntoDocument(<Root />);
-const leaf = ReactTestUtils.findAllInRenderedTree(tree, (component) => {
-	return component.constructor.name === "Leaf";
-})[0];
 
 describe("Falcor", () => {
 	describe("React Integration", () => {
-		it("should get data from the model and pass it down to the component", (done) => {
+		it("should initialize a component with data retrieved from a model", (done) => {
 			setTimeout(() => {
 				try {
-					expect(leaf.props.model.foo.bar).to.equal("foo");
+					expect(FindLeaf(tree, "Leaf").props.data.foo.bar).to.equal("foo");
+					done();
+				} catch (error) {
+					done(error);
+				}
+			});
+		});
+		it("should initialize a component with data retrieved from a model and collapsed on a single property", (done) => {
+			setTimeout(() => {
+				try {
+					expect(FindLeaf(tree, "LeafSafe").props.falcor.data.foo.bar).to.equal("foo");
+					done();
+				} catch (error) {
+					done(error);
+				}
+			});
+		});
+		it("should initialize a component with data retrieved from a model and spread on its properties", (done) => {
+			setTimeout(() => {
+				try {
+					expect(FindLeaf(tree, "LeafUnsafe").props.foo.bar).to.equal("foo");
+					done();
+				} catch (error) {
+					done(error);
+				}
+			});
+		});
+		it("should reinitialize a component with data retrieved from a model after receiving new properties", async(done) => {
+			await FindLeaf(tree, "Root").setState({ foo: "foo" });
+			setTimeout(() => {
+				try {
+					expect(FindLeaf(tree, "LeafWithProps").props.data.foo.foo).to.equal("bar");
 					done();
 				} catch (error) {
 					done(error);
