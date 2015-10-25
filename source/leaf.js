1b0ef7677b5ca4a5b9277145ddc9616150351f01
Giovanni Calò <giovanni.calo@live.it>
2015-10-25 20:41:38 +0000

    Greatly improved leaf decorator, added properties safety option, changed to propagate properties, added Falcor model methods wrappers

--- source/leaf.js
+++ source/leaf.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function(query) {
+export default function(query, config = { propsSafety: 1 }) {
 	return function(component) {
 		const Component = component;
 		return class FalcorLeaf extends React.Component {
 
 			constructor(props) {
 				super(props);
-				this.state = { data: null };
+				this.state = {};
 			}
 
 			static contextTypes = {
@@ -15,24 +15,74 @@ export default function(query) {
 			};
 
 			componentWillMount() {
-				this.executeQuery();
+				this.initialize();
 			}
 
-			componentWillReceiveProps() {
-				this.executeQuery();
+			componentWillReceiveProps(props) {
+				this.initialize(props);
 			}
 
-			async executeQuery() {
+			async call(functionPath, args, refSuffixes, thisPaths) {
 				try {
-					const data = await this.context.model.get(...query());
-					this.setState({ data: data && data.json });
+					return await this.context.model.call(functionPath, args, refSuffixes, thisPaths);
+				} catch (error) {
+					console.log(error);
+				}
+			}
+
+			async get(...pathSets) {
+				try {
+					return await this.context.model.get(...pathSets);
+				} catch (error) {
+					console.log(error);
+				}
+			}
+
+			getChildProps() {
+				const props = {
+					falcor: {
+						call: this.call,
+						data: this.state,
+						get: this.get,
+						set: this.set
+					}
+				};
+				if (config.propsSafety < 2) {
+					Object.entries(props.falcor).map(([key, value]) => {
+						props[key] = value;
+					});
+					delete props.falcor;
+					if (config.propsSafety < 1) {
+						delete props.data;
+						Object.entries(this.state).map(([key, value]) => {
+							props[key] = value;
+						});
+					}
+				}
+				return props;
+			}
+
+			async initialize(props = this.props) {
+				try {
+					const data = await this.get(...query(props));
+					if (data && data.json) {
+						this.setState(data.json);
+					}
 				} catch (error) {
 					console.log(error);
 				}
 			}
 
 			render() {
-				return <Component model={this.state.data} />;
+				return <Component {...this.props} {...this.getChildProps()} />;
+			}
+
+			async set(...pathValues) {
+				try {
+					return await this.context.model.set(...pathValues);
+				} catch (error) {
+					console.log(error);
+				}
 			}
 
 		};
