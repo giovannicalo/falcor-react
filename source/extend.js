c8f3e06608b717eaf98441987718039312c14665
Giovanni Calò <giovanni.calo@live.it>
2015-10-27 04:27:32 +0000

    Added extend module

---
+++ source/extend.js
@@ -0,0 +1,18 @@
+export default function extend(destination, ...sources) {
+	if (destination && destination.constructor === Object) {
+		sources.forEach((source) => {
+			Object.entries(source).forEach(([key, value]) => {
+				if (destination[key] && destination[key].constructor === Object && value.constructor === Object) {
+					extend(destination[key], value);
+				} else {
+					destination[key] = value;
+				}
+			});
+		});
+		return destination;
+	} else if (sources.length) {
+		return sources[sources.length - 1];
+	} else {
+		return destination;
+	}
+}
