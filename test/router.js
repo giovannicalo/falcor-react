49a02fbdebd65fbcb60dc8fc2c882a4b20b766e7
Giovanni Calò <giovanni.calo@live.it>
2015-11-02 04:47:34 +0000

    Added test router

---
+++ test/router.js
@@ -0,0 +1,46 @@
+import Falcor from "falcor";
+import FalcorRouter from "falcor-router";
+
+export default new FalcorRouter([{
+	get() {
+		return {
+			jsonGraph: {
+				bar: {
+					1: { bar: "foo", foo: "bar" },
+					2: { bar: "foo", foo: "bar" },
+					3: { bar: "foo", foo: "bar" }
+				}
+			}
+		};
+	},
+	route: "bar[{integers}][\"bar\", \"foo\"]",
+	set(jsonGraph) {
+		return { jsonGraph };
+	}
+}, {
+	get() {
+		return {
+			jsonGraph: {
+				foo: {
+					bar: {
+						0: Falcor.Model.ref(["bar", 1]),
+						1: Falcor.Model.ref(["bar", 2]),
+						2: Falcor.Model.ref(["bar", 3])
+					}
+				}
+			}
+		};
+	},
+	route: "foo.bar[{integers}]",
+	set(jsonGraph) {
+		return { jsonGraph };
+	}
+}, {
+	call(path, parameters) {
+		return [{
+			path: ["bar", 4, "foo"],
+			value: parameters && parameters[0]
+		}];
+	},
+	route: "foo.foo"
+}]);
