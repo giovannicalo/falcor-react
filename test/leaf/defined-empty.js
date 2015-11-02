db67f0b0f486f3c487dd9ca8478c3e0fef81ff71
Giovanni Calò <giovanni.calo@live.it>
2015-11-02 04:48:36 +0000

    Removed unneeded path

--- test/leaf/defined-empty.js
+++ test/leaf/defined-empty.js
@@ -5,8 +5,7 @@ import FalcorReact from "../../source";
 @FalcorReact.Leaf([
 	["bar", { from: 0, to: 9 }, "bar"],
 	["bar", 3, "foo"],
-	["foo", "bar", { length: 10 }, ["bar", "foo"]],
-	["foo", "foo"]
+	["foo", "bar", { length: 10 }, ["bar", "foo"]]
 ], { defineEmpty: true })
 export default class LeafDefinedEmpty extends React.Component {
 
