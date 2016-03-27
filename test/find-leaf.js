744f86adc3449646baf1cf462d17441013688df7
Giovanni Calò <giovanni.calo@live.it>
2016-03-27 03:15:40 +0100

    Improved leaf find function, added JSDoc

--- test/find-leaf.js
+++ test/find-leaf.js
@@ -1,12 +1,20 @@
 import ReactTestUtils from "react-addons-test-utils";
 
-export default function(tree, name) {
+/**
+ * Finds the first leaf with a given name in a React tree
+ *
+ * @export
+ * @param {Object} tree - A React tree
+ * @param {String} name - The name of the leaf to find
+ * @returns {Object} The leaf
+ */
+export default (tree, name) => {
 	const leaf = ReactTestUtils.findAllInRenderedTree(tree, (leaf) => {
 		return leaf.constructor.name === name;
 	});
 	if (Array.isArray(leaf) && leaf.length) {
 		return leaf[0];
 	} else {
-		throw new Error("Leaf " + name + " not found");
+		throw new Error(`Leaf ${name} not found`);
 	}
-}
+};
