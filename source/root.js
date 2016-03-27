96054e54d42df217d779528969b18d836cabf22f
Giovanni Calò <giovanni.calo@live.it>
2016-03-27 04:32:21 +0100

    Improved root decorator code slightly, added JSDoc

--- source/root.js
+++ source/root.js
@@ -1,8 +1,15 @@
 import Falcor from "falcor";
 import React from "react";
 
-export default function(config) {
-	return function(component) {
+/**
+ * Wraps a React component and binds it to a Falcor model
+ *
+ * @export
+ * @param {Object} config - A Falcor model configuration object
+ * @returns {function} The wrapped component
+ */
+export default (config) => {
+	return (component) => {
 		const Component = component;
 		return class FalcorRoot extends React.Component {
 
@@ -15,7 +22,7 @@ export default function(config) {
 				model: React.PropTypes.object.isRequired
 			};
 
-			static displayName = "Falcor Root (" + (component.displayName || component.name || "Anonymous Component") + ")"
+			static displayName = `Falcor Root (${component.displayName || component.name || "Anonymous Component"})`
 
 			getChildContext() {
 				return { model: this.state.model };
@@ -27,4 +34,4 @@ export default function(config) {
 
 		};
 	};
-}
+};
